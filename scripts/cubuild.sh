e983697af4cc3dd41c48e8b0ac98ea230c3dfbc6
delleceste <delleceste@gmail.com>
2018-05-08 15:35:59 +0200

    ld.so.conf support in cubuild.sh

--- scripts/cubuild.sh
+++ scripts/cubuild.sh
@@ -727,13 +727,13 @@ if [ $make_install -eq 1 ]; then
 
 	
 	if [ $tango -eq 1 ]; then
-		echo -e "\e[0;32m\n*\n* INSTALL \e[0m to get started, type"
+		echo -e "\e[0;32m\n*\n* INSTALL \e[0m to get started, after making sure a TangoTest device instance is running on a given *tango-db-host* *Tango* database, type"
 		echo -e "  export TANGO_HOST=tango-db-host:PORT" 
 		echo -e "  cumbia client sys/tg_test/1/double_scalar sys/tg_test/1/long_scalar"
-		echo -e "\e[0;32m\n*\n*\e[0m\n"
+		echo -e "\e[0;32m\n*\n*\e[0m"
 	fi
 
-	echo -e "\e[0;32m\n*\n* INSTALL \e[1;32myou may need to execute\n*\n  \e[1;36msource  /etc/profile\e[1;32m \n*"
+	echo -e "\e[0;32m*\n* INSTALL \e[1;32myou may need to execute\n*\n  \e[1;36msource  /etc/profile\e[1;32m \n*"
 	echo -e "* to enable shortcuts for cumbia apps. Then type \n*\n  \e[1;36mcumbia\e[1;32m\n*\n* to list available options\n*\e[0m"
 
 fi
