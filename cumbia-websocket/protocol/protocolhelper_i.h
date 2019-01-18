7cc20ca309afdc8fd6c4df58332183bc6ef5f8e1
Giacomo S <giacomo.strangolino@elettra.eu>
2019-01-18 10:12:56 +0100

    added missing cumbia websocket protocol/protocolhelper_i.h to git

---
+++ cumbia-websocket/protocol/protocolhelper_i.h
@@ -0,0 +1,21 @@
+#ifndef PROTOCOLHELPER_I_H
+#define PROTOCOLHELPER_I_H
+
+#include <string>
+
+class ProtocolHelper_I
+{
+public:
+
+    virtual ~ProtocolHelper_I() {}
+
+    virtual int dataFormatStrToInt(const std::string& fmt) const = 0;
+
+    virtual std::string dataFormatToStr(int fmt)  const = 0;
+
+    virtual std::string dataTypeToStr(int dt) const = 0;
+
+    virtual int dataTypeStrToInt(const std::string &dt ) const = 0;
+};
+
+#endif // PROTOCOLHELPER_I_H
