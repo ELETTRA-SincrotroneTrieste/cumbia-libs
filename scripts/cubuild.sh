bf57f2be948ac295eddb271b7337bf3ead99d889
Giacomo S <giacomo.strangolino@elettra.eu>
2019-08-13 16:34:54 +0200

    cubuild meson configure command line option change

--- scripts/cubuild.sh
+++ scripts/cubuild.sh
@@ -525,6 +525,7 @@ for x in "${meson_p[@]}" ; do
                         echo -e  "\e[1;32m* \e[0mEPICS_HOST_ARCH=\e[1;36m${EPICS_HOST_ARCH}\e[0m"
                         echo -e "\e[1;32m* \e[0m"
 
+                        meson configure -Depics_base=${EPICS_BASE} -Depics_host_arch=${EPICS_HOST_ARCH}
                         meson configure -Depics_base=${EPICS_BASE} -Depics_host_arch=${EPICS_HOST_ARCH} -Dprefix=$tmp_installdir -Dlibdir=$lib_dir -Dbuildtype=$build_type && ninja
                         meson configure -Depics_base=${EPICS_BASE} -Depics_host_arch=${EPICS_HOST_ARCH} -Dprefix=$tmp_installdir -Dlibdir=$lib_dir -Dbuildtype=$build_type && ninja install
 
