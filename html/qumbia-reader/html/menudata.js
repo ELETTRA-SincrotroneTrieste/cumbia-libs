b90bc38cd7f13366b344271b00b6c3190e389857
Giacomo S <giacomo.strangolino@elettra.eu>
2019-12-06 10:59:09 +0100

    doc regen

--- html/qumbia-reader/html/menudata.js
+++ html/qumbia-reader/html/menudata.js
@@ -59,6 +59,7 @@ var menudata={children:[
 {text:"q",url:"functions_func.html#index_q"},
 {text:"r",url:"functions_func.html#index_r"},
 {text:"s",url:"functions_func.html#index_s"},
+{text:"t",url:"functions_func.html#index_t"},
 {text:"u",url:"functions_func.html#index_u"},
 {text:"~",url:"functions_func.html#index__7E"}]},
 {text:"Variables",url:"functions_vars.html"},
