db0deb70c6d15be5f8f4bfacabc21527ca1747f1
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2020-05-22 16:36:13 +0200

    print test for events

--- cumbia-tango/lib/cueventactivity.cpp
+++ cumbia-tango/lib/cueventactivity.cpp
@@ -295,7 +295,8 @@ void CuEventActivity::push_event(Tango::EventData *e)
         d["err"] = true;
         d["msg"] = utils.strerror(e->errors);
     }
-    cuprintf("CuEventActivity.push_event for %s val %s\n", vtos(v, "value"), vtos(v, "src"));
+
+    cuprintf("CuEventActivity.push_event for %s val %s\n", d["src"].toString().c_str(), d["value"].toString().c_str());
     publishResult(d);
 }
 
