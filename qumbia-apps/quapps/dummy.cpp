a5c5824683f84544f7727a9de39d34873b6f4e80
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2020-05-22 17:17:33 +0200

    added dummy.cpp

---
+++ qumbia-apps/quapps/dummy.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("\n");
+	printf("this is a dummy application. The project rather installs bash scripts\n");
+	printf("\n* please run make install as root or \"sudo make install\" to install\n"
+		"  the provided bash scripts\n");
+	printf("\n* you can also read the README file.\n\n");
+	return EXIT_SUCCESS;
+}
