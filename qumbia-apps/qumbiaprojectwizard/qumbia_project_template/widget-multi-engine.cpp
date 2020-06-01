7a2d564d8d71a872843cc6f4848cc18a7d9b1dde
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2020-06-01 12:02:22 +0200

    updated to module selector

--- qumbia-apps/qumbiaprojectwizard/qumbia_project_template/widget-multi-engine.cpp
+++ qumbia-apps/qumbiaprojectwizard/qumbia_project_template/widget-multi-engine.cpp
@@ -12,7 +12,7 @@ $MAINCLASS$::$MAINCLASS$(CumbiaPool *cumbia_pool, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::$UIFORMCLASS$)
 {
-    CuModuleLoader mloader(cumbia_pool, m_ctrl_factory_pool, &m_log_impl);
+    CuModuleLoader mloader(cumbia_pool, &m_ctrl_factory_pool, &m_log_impl);
     ui->setupUi(this, cu_pool, m_ctrl_factory_pool);
 
     // mloader.modules() to get the list of loaded modules
