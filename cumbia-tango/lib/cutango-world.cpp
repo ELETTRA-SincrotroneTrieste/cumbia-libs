6fefd8ae28448a74b85cec621e22f08b0ff7cfa2
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2020-06-26 11:38:16 +0200

    polling activity: removed exception print and decrease polling msg shortened

--- cumbia-tango/lib/cutango-world.cpp
+++ cumbia-tango/lib/cutango-world.cpp
@@ -653,7 +653,7 @@ bool CuTangoWorld::read_att(Tango::DeviceProxy *dev, const string &attribute, Cu
 
     try
     {
-        if(res["src"].toString().find("p/mod/hv_p2") != std::string::npos)
+//        if(res["src"].toString().find("p/mod/hv_p2") != std::string::npos)
                 printf("CuTangoWorld::read_att reading %s\e[0m\n", res["src"].toString().c_str());
         std::string att(attribute);
         Tango::DeviceAttribute da = dev->read_attribute(att);
@@ -664,8 +664,8 @@ bool CuTangoWorld::read_att(Tango::DeviceProxy *dev, const string &attribute, Cu
         d->error = true;
         d->message = strerror(e);
         res.putTimestamp();
-        if(res["src"].toString().find("p/mod/hv_p2") != std::string::npos)
-            cuprintf("\e[1;31;31mCuTangoWorld.read_att: attribute ERRROR %s : %s\e[0m\n", attribute.c_str(),
+//        if(res["src"].toString().find("p/mod/hv_p2") != std::string::npos)
+            printf("\e[1;31;31mCuTangoWorld.read_att: attribute ERRROR %s : %s\e[0m\n", attribute.c_str(),
                  d->message.c_str());
     }
     res["err"] = d->error;
