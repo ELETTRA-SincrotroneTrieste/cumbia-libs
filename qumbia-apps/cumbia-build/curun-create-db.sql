16902e410ed03460862bab60494e4472c1f4f4b7
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2021-01-02 15:08:25 +0100

    added sqlite python script and create sql

---
+++ qumbia-apps/cumbia-build/curun-create-db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS apps (
+	name TEXT NOT NULL,
+	path TEXT NOT NULL,
+	PRIMARY KEY(name,path)
+);
+
+CREATE TABLE IF NOT EXISTS history (
+	id INT NOT NULL,
+	env TEXT,
+	args TEXT,
+	datetime DATETIME NOT NULL,
+    PRIMARY KEY(id,env,args)
+);
+
+
+CREATE TABLE IF NOT EXISTS test (
+	id INT NOT NULL,
+	env TEXT,
+	args TEXT,
+	datetime DATETIME NOT NULL,
+    PRIMARY KEY(id,env,args)
+);
+
+
