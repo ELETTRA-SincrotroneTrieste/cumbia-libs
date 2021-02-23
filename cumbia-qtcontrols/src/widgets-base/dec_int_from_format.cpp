5eac1e1c962c95a3796e0eb213ac06614b36e56e
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2021-02-23 13:25:28 +0100

    removed prints and some indentation changes

--- cumbia-qtcontrols/src/widgets-base/dec_int_from_format.cpp
+++ cumbia-qtcontrols/src/widgets-base/dec_int_from_format.cpp
@@ -17,7 +17,7 @@ DecIntFromFormat::DecIntFromFormat(QString f)
 bool DecIntFromFormat::decode()
 {
     int pos = - 1;
-    printf("\e[1;36mDecIntFromFormat::decode() format %s\e[0m\n", qstoc(d_format));
+//    printf("\e[1;36mDecIntFromFormat::decode() format %s\e[0m\n", qstoc(d_format));
     /* add ' in [0-9] to recognize "%'d" */
     QRegExp intRe("%(\\d+)d\\b");
     pos = intRe.indexIn(d_format);
@@ -26,57 +26,43 @@ bool DecIntFromFormat::decode()
         d_decDefaults = false;
         d_decDigits = 0;
         d_intDefaults = intRe.capturedTexts().size() == 0;
-        printf("DecIntFromFormat::decode d_intDefaults: %d\n", d_intDefaults);
         if(!d_intDefaults) {
 
             d_intDigits = intRe.capturedTexts().at(1).toInt();
-            printf("DecIntFromFormat::decode d_intDigits : %d\n", d_intDigits);
             d_intDefaults = false;
         }
     }
-    else
-    {
+    else {
         if(d_format == "%f")
             return true;
         QRegExp floatRe("%[0-9]*\\.{1,1}[0-9]+f\\b");
         pos = floatRe.indexIn(d_format);
-        if(pos >= 0)
-        {
+        if(pos >= 0) {
             /* type %.3f  decimal digits only */
             QRegExp flRe1("%\\.{1,1}([0-9]+)f\\b");
             /* type %2.3f integer and decimal digits */
             QRegExp flRe2("%([0-9]+)\\.{1,1}([0-9]+)f\\b");
             pos = flRe1.indexIn(d_format);
-            if(pos >= 0 && flRe1.capturedTexts().size() > 1)
-            {
+            if(pos >= 0 && flRe1.capturedTexts().size() > 1) {
                 d_decDigits = flRe1.capturedTexts().at(1).toInt();
                 d_decDefaults = false;
             }
-            else // %x.yf
-            {
+            else { // %x.yf
                 pos = flRe2.indexIn(d_format);
-                if(pos >= 0 && flRe2.capturedTexts().size() > 2)
-                {
-                    printf("\e[1;36mDecIntFromFormat::decode() captured %d int %d dec d_dec defaults %d d_intDefaults %d\n",
-                        flRe2.capturedTexts().at(1).toInt(), flRe2.capturedTexts().at(2).toInt(), d_decDefaults, d_intDefaults);
-
+                if(pos >= 0 && flRe2.capturedTexts().size() > 2) {
                     d_decDefaults = false;
                     d_decDigits = flRe2.capturedTexts().at(2).toInt();
                     d_intDigits = flRe2.capturedTexts().at(1).toInt() - d_decDigits - ( d_decDigits > 0 ? 1 : 0);
                     d_intDefaults = d_intDigits <= 0;
-                    printf("\e[1;36mDecIntFromFormat::decode() int %d dec %d int def %d dec default %d\n",
-                           d_intDigits, d_decDigits, d_decDefaults, d_intDefaults);
                 }
             }
         }
     }
-    if(pos < 0)
-    {
-        printf("\e[1;31m* \e[1;36mDecIntFromFormat::decode()\e[0m format error in \"%s\"", qstoc(d_format));
+    if(pos < 0) {
+        perr("DecIntFromFormat::decode(): error or unsupported format \"%s\"", qstoc(d_format));
         return false;
     }
-    else
-        return true;
+    return true;
 }
 
 
