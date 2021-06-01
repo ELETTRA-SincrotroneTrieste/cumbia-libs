04b44a3773f8cff5806d79dcaaf7fb21e0230685
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2021-06-01 09:52:22 +0200

    fix in print debug

--- cumbia-tango/lib/cutreader.cpp
+++ cumbia-tango/lib/cutreader.cpp
@@ -97,7 +97,8 @@ void CuTReader::onResult(const CuData &data) {
         if(event_subscribe_fail)
             printf("CuTReader.onResult: \e[1;35m: polling fallback after \e[1;34;35mevent subscription failure '%s'\e[0m\n", vtoc2(data, "src"));
         if(data.containsKey("ev_except") && data["ev_except"].toBool()) {
-            printf("CuTReader.onResult: \e[1;31m: polling fallback after *** \e[1;31;35mevent failure src %s msg %s\e[0m\n", vtoc2(data, "src"), vtoc2(data, "msg"));
+            if(!event_subscribe_fail) // DEBUG PRINT only if !event_subs_fail
+                printf("CuTReader.onResult: \e[1;31m: polling fallback after *** \e[1;31;35mevent failure src %s msg %s\e[0m\n", vtoc2(data, "src"), vtoc2(data, "msg"));
             m_polling_fallback();
         }
     }
