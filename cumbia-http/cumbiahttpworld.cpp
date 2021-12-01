1613b20a0afb45204c26a2a9a5f496dc99164ce6
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2021-12-01 17:14:29 +0100

    removed printf from json_decode

--- cumbia-http/cumbiahttpworld.cpp
+++ cumbia-http/cumbiahttpworld.cpp
@@ -70,7 +70,6 @@ bool CumbiaHTTPWorld::request_reverse_eng(const QString &json, QMap<QString, QSt
 }
 
 bool CumbiaHTTPWorld::json_decode(const QByteArray &ba, std::list<CuData> &out) const {
-    printf("\e[1;36mCumbiaHttpWorld.json_decode from %s\e[0m\n", ba.data());
     QJsonParseError pe;
     QJsonDocument json = QJsonDocument::fromJson(ba, &pe);
     if(pe.error != QJsonParseError::NoError) {
