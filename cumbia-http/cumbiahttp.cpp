2a9f4db35ebe59315eecc8ebc866d64c930cbecc
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2022-01-21 16:43:23 +0100

    send id manager unsubscribe only if client id > -1

--- cumbia-http/cumbiahttp.cpp
+++ cumbia-http/cumbiahttp.cpp
@@ -93,7 +93,7 @@ CumbiaHttp::~CumbiaHttp()
     QList<SrcItem> ri, wi;
     d->src_q_man->dequeueItems(ri, wi);
     onSrcBundleReqReady(ri, wi);
-    if(d->client_id > -1)
+    if(d->client_id > 0)
         d->id_man->unsubscribe(true); // true: block
     /* all registered services are unregistered and deleted by cumbia destructor after threads have joined */
     if(d->m_threadsEventBridgeFactory)
