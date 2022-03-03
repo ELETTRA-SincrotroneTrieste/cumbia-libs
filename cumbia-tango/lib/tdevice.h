2b47298904ec10d786f6486578f710ae2927ff9f
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2022-03-03 13:00:25 +0100

    removed newlines

--- cumbia-tango/lib/tdevice.h
+++ cumbia-tango/lib/tdevice.h
@@ -13,24 +13,16 @@ class TDevice
 {
     friend class  CuDeviceFactoryService;
 public:
-
     Tango::DeviceProxy *getDevice() const;
-
     std::string getMessage() const;
-
     bool isValid() const;
-
     bool operator ==(const TDevice &other) const;
-
     std::string getName() const;
-
     int refCnt() const;
-
 private:
     TDevicePrivate *d;
 
     void addRef();
-
     int removeRef();
 
     ~TDevice();
