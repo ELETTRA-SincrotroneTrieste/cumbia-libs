22a113b626ba7d7e9ac2f715eba5819f62cfaf2a
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2022-03-03 13:07:58 +0100

    documentation about CuPollDataUpdatePolicy

--- cumbia-tango/lib/cupollingservice.h
+++ cumbia-tango/lib/cupollingservice.h
@@ -11,6 +11,27 @@ class TSource;
 class CuTangoActionI;
 class CuData;
 
+/*! \brief CumbiaTango polling service. Mainly used internally, it can be accessed to specify a particular data update policy.
+ *
+ * \par Update policy while data remains constant.
+ *
+ *  CuPollingService can now avoid publishing results when data does not change. This can save a lot of time to applications
+ *  reading from several *polled sources*.
+ *
+ *  Three types of *update policy* are available for *polled sources*:
+ *
+ *   1. Always update, even though the data does not change (default usual behaviour)
+ *   2. Update the timestamp only as long as data does not change;
+ *   3. Do not perform any update as long as data does not change. Updates are not sent
+ *      at the *activity* level. This means event posting on the application thread is
+ *      completely skipped, ensuring maximum performance.
+ *
+ *  Changes in error flags, messages or Tango data quality are always notified.
+ *
+ *  The update policy shall be specified using setDataUpdatePolicy
+ *
+ *  @see CuPollDataUpdatePolicy
+*/
 class CuPollingService : public CuServiceI
 {
 public:
