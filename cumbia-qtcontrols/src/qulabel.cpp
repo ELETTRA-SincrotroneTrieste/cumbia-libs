3aa2ab0823c09723257a995be0ae590efb92f0c1
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2022-05-10 15:21:38 +0200

    QuLabel.setSource operates only if new src != current

--- cumbia-qtcontrols/src/qulabel.cpp
+++ cumbia-qtcontrols/src/qulabel.cpp
@@ -197,9 +197,11 @@ QuPalette QuLabel::quPalette() const {
  * @see source
  */
 void QuLabel::setSource(const QString &s) {
-    CuControlsReaderA * r = d->context->replace_reader(s.toStdString(), this);
-    if(r)
-        r->setSource(s);
+    if(s != source()) {
+        CuControlsReaderA * r = d->context->replace_reader(s.toStdString(), this);
+        if(r)
+            r->setSource(s);
+    }
 }
 
 void QuLabel::unsetSource()
