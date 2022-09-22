2e6534e295811c730595efe7bdd74e01af367075
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2022-09-22 15:28:12 +0200

    added files engineswitchdialogextension cuengine_hot_switch

---
+++ qumbia-plugins/actions-extension/engineswitchdialogextension.cpp
@@ -0,0 +1,53 @@
+#include "engineswitchdialogextension.h"
+#include "cuengineswitchdialog.h"
+
+class EngineSwitchDialogExtensionPrivate {
+public:
+    EngineSwitchDialogExtensionPrivate(const CuContextI* ci) : ctxi(ci) {}
+    const CuContextI * ctxi;
+    std::string msg;
+};
+
+EngineSwitchDialogExtension::EngineSwitchDialogExtension(const CuContextI *ctx_i, QWidget* parent )
+    : QObject{parent} {
+    d = new EngineSwitchDialogExtensionPrivate(ctx_i);
+}
+
+EngineSwitchDialogExtension::~EngineSwitchDialogExtension() {
+    delete d;
+}
+
+QString EngineSwitchDialogExtension::getName() const {
+    return "EngineSwitchDialogExtension";
+}
+
+CuData EngineSwitchDialogExtension::execute(const CuData &in, const CuContextI *ctxI) {
+//#if defined QUMBIA_TANGO_CONTROLS_VERSION && defined CUMBIA_HTTP_VERSION
+    // WA_DeleteOnClose attribute is set
+    printf("\e[1;32;3mexecuting exec CuEngineSwitchDialog\e[0m\n\n\n");
+    CuEngineSwitchDialog* dlg = new CuEngineSwitchDialog(nullptr);
+    dlg->exec(in, ctxI);
+//#else
+//#endif
+    return CuData();
+}
+
+std::vector<CuData> EngineSwitchDialogExtension::execute(const std::vector<CuData> &, const CuContextI *) {
+    return std::vector<CuData>();
+}
+
+QObject *EngineSwitchDialogExtension::get_qobject() {
+    return this;
+}
+
+const CuContextI *EngineSwitchDialogExtension::getContextI() const {
+    return nullptr;
+}
+
+std::string EngineSwitchDialogExtension::message() const {
+    return d->msg;
+}
+
+bool EngineSwitchDialogExtension::error() const {
+    return d->msg.size() > 0;
+}
---
+++ qumbia-plugins/actions-extension/engineswitchdialogextension.h
@@ -0,0 +1,33 @@
+#ifndef ENGINESWITCHDIALOGEXTENSION_H
+#define ENGINESWITCHDIALOGEXTENSION_H
+
+#include <QWidget>
+#include <quaction-extension-plugininterface.h>
+
+class EngineSwitchDialogExtensionPrivate;
+
+class EngineSwitchDialogExtension : public QObject,  public QuActionExtensionI
+{
+    Q_OBJECT
+public:
+    explicit EngineSwitchDialogExtension(const CuContextI *ctx, QWidget* parent = nullptr);
+    virtual ~EngineSwitchDialogExtension();
+
+signals:
+
+
+    // QuActionExtensionI interface
+public:
+    QString getName() const;
+    CuData execute(const CuData &in, const CuContextI *ctx);
+    std::vector<CuData> execute(const std::vector<CuData> &in_list, const CuContextI *ctx);
+    QObject *get_qobject();
+    const CuContextI *getContextI() const;
+    std::string message() const;
+    bool error() const;
+
+private:
+    EngineSwitchDialogExtensionPrivate *d;
+};
+
+#endif // ENGINESWITCHDIALOGEXTENSION_H
