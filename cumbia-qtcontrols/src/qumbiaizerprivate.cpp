db6d7e3f857cb754b5281ebdbfd4c0bc1b101c0c
Giacomo Strangolino <giacomo.strangolino@elettra.eu>
2023-05-29 16:42:38 +0200

    implemented attach on QuWatcher for CuData arg slot

--- cumbia-qtcontrols/src/qumbiaizerprivate.cpp
+++ cumbia-qtcontrols/src/qumbiaizerprivate.cpp
@@ -37,7 +37,6 @@ bool QumbiaizerPrivate::inTypeOfMethod(const QString &method, QObject *obj, char
         if(parTypes.size() == 1)
         {
             strncpy(in_type, parTypes.first().constData(), TYPELEN - 1);
-            printf("QumbiaizerPrivate.inType of method in_type %s\n", in_type);
             return true;
         }
         else if(parTypes.size() == 0) /* void */
